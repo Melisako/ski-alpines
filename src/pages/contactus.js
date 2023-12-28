9443be12e965bcd9976dca4ac48f9d4a06472d48
Melisako <120452771+Melisako@users.noreply.github.com>
2023-12-28 01:32:55 +0100

    js-commit

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -5,7 +5,7 @@ import '../assets/contactus.css';
 const ContactUs = () => {
     return (
       <div className="contact-us">
-        <h2>Contact Us</h2>
+        <h2>Kontaktiere uns</h2>
         <form className="contact-form">
           <div className="form-group">
             <label>Name:</label>
@@ -16,20 +16,20 @@ const ContactUs = () => {
             <input type="email" className="form-control" />
           </div>
           <div className="form-group">
-            <label className="mess">Memo:</label>
+            <label className="mess">Notiz:</label>
             <input type="message" className="form-cont" rows="5"></input>
           </div>
           <div className="date-group">
             <div className="form-group">
-              <label className="date-label">Start Date:</label>
+              <label className="date-label">Startdatum:</label>
               <input type="date" className="form-control start-date" />
             </div>
             <div className="form-group">
-              <label className="date-label">End Date:</label>
+              <label className="date-label">Enddatum:</label>
               <input type="date" className="form-control end-date" />
             </div>
           </div>
-          <button type="submit" className="btn-submit">Submit</button>
+          <button type="submit" className="btn-submit">Absenden</button>
         </form>
       </div>
     );
